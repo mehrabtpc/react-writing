746f8cd4cc9c5a0c0bc2903a598a9893fe8affcb
mehrab <mehrabt.pc@gmail.com>
2024-06-05 15:28:10 +0330

    create writings route,logo

---
+++ src/pages/writing/index/index.jsx
@@ -0,0 +1,17 @@
+/* todo : implement first */
+import React from "react";
+
+export const Writing = () => {
+  return (
+    <div className="shop">
+      <div className="shopTitle">
+        <h1>PedroTech Shop</h1>
+      </div>
+
+      <div className="writings">
+        <h1>hello</h1>
+
+      </div>
+    </div>
+  );
+};
